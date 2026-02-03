10cb45219935131ee913213ac7e651527fdfd3b5
MSKnoGHB <242181889+MSKnoGHB@users.noreply.github.com>
2026-02-03 14:58:55 +0000

    [Wip]アソシエーション実装中

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :user_image
+  has_many :room_accesses, dependent: :destroy
+  has_many :chat_logs, dependent: :destroy
+  has_many :study_records, dependent: :destroy
+  has_many :study_themes, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :active_relationships, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Follow", foreign_key: "followee_id", dependent: :destroy
 end
