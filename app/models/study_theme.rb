ae63c32fbdf7a0add880daa2cea76cf7f6a0f046
MSKnoGHB <242181889+MSKnoGHB@users.noreply.github.com>
2026-02-09 03:44:36 +0000

    [Finish]テーマ色enumとi18nの実装

--- app/models/study_theme.rb
+++ app/models/study_theme.rb
@@ -7,4 +7,16 @@ class StudyTheme < ApplicationRecord
   validates :theme_title, presence: true, uniqueness:{ scope: :user_id }, length: { maximum: 20 }
   validates :theme_body, length: { maximum: 100 }
 
+  enum theme_color: {
+    red:    0, 
+    orange: 1, 
+    yellow: 2, 
+    green:  3, 
+    blue:   4, 
+    purple: 5, 
+    pink:   6, 
+    brown:  7, 
+    black:  8
+  }
+
 end
