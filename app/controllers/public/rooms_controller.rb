d3331a5ccc968f1262d672cf01d9a41dbf62274c
MSKnoGHB <242181889+MSKnoGHB@users.noreply.github.com>
2026-02-14 04:36:34 +0000

    [Finish］画面遷移とデータ格納機能実装

--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -1,9 +1,11 @@
 class Public::RoomsController < ApplicationController
   def index
+    #学習ルーム選択
     @rooms = Room.all
   end
 
   def show
+    #room_idの受け渡し
     @room = Room.find(params[:id])
 
     #modal_テーマ選択後の入室
@@ -14,17 +16,10 @@ class Public::RoomsController < ApplicationController
     @study_theme = StudyTheme.new
     @study_categories = StudyCategory.all
  
-    
-   
-    
-    
-    @select_study_theme = StudyTheme.find_by(params[:study_theme_id])
-    
     #room_accessを一意にする
     @active_room_access = current_user.room_accesses.find_by(is_active: true)
     #ボタン表示を制御するために現状の学習ステータスを格納する
     @study_status = @active_room_access&.study_status
-    
     #study_recordを一意にする
     @study_record = current_user.study_records.find_by(ended_at: nil)
   end
