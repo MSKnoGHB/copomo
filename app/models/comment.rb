9bcd1e21df6d24f22b8ea763bd4c65a8e43307d0
MSKnoGHB <242181889+MSKnoGHB@users.noreply.github.com>
2026-04-29 15:10:56 +0000

    WIP: admin flash messages & validation errors (partial)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :study_record
 
-  validates :comment_body, presence: true, length: { maximum: 250 }
+  COMMENT_MAX_LENGTH = 250
+  validates :comment_body, presence: true, length: { maximum: COMMENT_MAX_LENGTH }
 end
